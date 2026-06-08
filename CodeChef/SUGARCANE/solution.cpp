8ae39f6d50727638d7f49cccf767c4aa8f07ed0e
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-08 12:39:11 +0530

    CodeChef: SUGARCANE - Accepted Solution

---
+++ CodeChef/SUGARCANE/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits
+    /stdc++.h>
+using namespace 
+    std;
+
+int main() {
+    // your 
+       code 
+       goes 
+       here
+    int t; 
+     cin>>t;
+      while(t
+       --){
+          long 
+       long n;
+          cin
+       >>n;
+          
+          cout
+       <<15*n
+       <<endl;
