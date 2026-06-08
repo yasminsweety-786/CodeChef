175ad4c20997128ab8c501641e982f9ec3bcf87f
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-08 13:06:04 +0530

    CodeChef: NOTEBOOK - Accepted Solution

--- CodeChef/NOTEBOOK/solution.cpp
+++ CodeChef/NOTEBOOK/solution.cpp
@@ -1,19 +1,14 @@
-#include <bits
-    /stdc++.h>
-using namespace 
-    std;
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    // your code 
-        goes here
+    // your code goes here
     int t;
     cin>>t;
     while(t--){
         int n;
         cin>>n;
-        cout<<n 
-        *10<<endl
-        ;
+        cout<<n *10<<endl;
     }
 
 }
