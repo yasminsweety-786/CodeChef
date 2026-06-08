41e1bd7273cb5fb772b5499a31b050b5ffd5f536
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-08 14:37:27 +0530

    CodeChef: THREETOPICS - Accepted Solution

---
+++ CodeChef/THREETOPICS.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  
+  if(d== a || d==b|| d==c)
+  cout<<"Yes"<<endl;
+  else {
+       cout<<"No"<<endl;
+  }
+}
