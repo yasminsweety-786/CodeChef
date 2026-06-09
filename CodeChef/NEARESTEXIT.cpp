da30d9405519e16d4d36be5a04a63d864b057932
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-09 17:53:38 +0530

    CodeChef: NEARESTEXIT - Accepted Solution

---
+++ CodeChef/NEARESTEXIT.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t; 
+cin>>t;
+ while ( t--){
+     int a;
+     cin>>a;
+      if ( a<=50){
+           cout<< "LEFT"<<endl;
+           
+      }
+      else  { 
+          cout<<"RIGHT"<<endl;
+ }
+}
+}
