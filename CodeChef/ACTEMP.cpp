73f30e2ea743add274b7944e70bc834b142fc094
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-09 17:47:51 +0530

    CodeChef: ACTEMP - Accepted Solution

---
+++ CodeChef/ACTEMP.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	 cin>>t;
+	  while(t--){
+	      int a,b,c;
+	      cin>>a>>b>>c;
+	      if ( a<=b && b>=c){
+	           cout<<"YES"<<endl;
+	      }
+	      else {
+	          cout<<"No"<<endl;
+	      }
+	  }
+
+}
