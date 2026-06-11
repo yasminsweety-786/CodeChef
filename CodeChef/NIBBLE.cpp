5e769c7053f4708ce0f9b5ecb7d6d1d834e60f71
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-12 04:13:25 +0530

    CodeChef: NIBBLE - Accepted Solution

---
+++ CodeChef/NIBBLE.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes hereI
+	int t;
+	cin>>t;
+	while (t--){
+	     int n;
+	     cin>>n;
+	     if ( n %4==0){
+	         cout<<"Good"<<endl;
+	     }
+	     else {
+	          cout<<"Not Good"<<endl;
+	     }
+	}
+
+}
