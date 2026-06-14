3e8da42a0bbe335ad1a07528d64ce83c3807793a
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-15 05:22:21 +0530

    CodeChef: MINCARS - Accepted Solution

---
+++ CodeChef/MINCARS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	 cin>>t;
+	  while(t--){
+	       int x;
+	       cin>>x;
+	       int cars = (x+3)/4;
+	       cout<<cars<<endl;
+	  }
+
+}
