9093ec88cde0b5df55ac608ec21768c70e7e7faa
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-16 04:47:35 +0530

    CodeChef: JENGA - Accepted Solution

---
+++ CodeChef/JENGA.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	 cin>>t;
+	 while(t--){
+	     int n,x;
+	     cin>>n>>x;
+	     if (x%n==0){
+	         cout<<"Yes"<<endl;
+	     }
+	     else {
+	          cout<<"No"<<endl;
+	     }
+	 }
+
+}
