9ff47cc4f43c2558f3ea20ed25b5542992a981a5
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-18 04:17:12 +0530

    CodeChef: DISCUS - Accepted Solution

---
+++ CodeChef/DISCUS.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	 cin>>t;
+	  while ( t--){
+	       int a,b,c;
+	       cin>>a>>b>>c;
+	        if ( a>b && a>c){
+	             cout<<a<<endl;
+	        }
+	        else if ( b>a && b>c){
+	             cout<<b<<endl;
+	        }
+	        else if ( a==b && b==c && c==a){
+	             cout<<a<<endl;
+	        }
+	        else {
+	             cout<<c<<endl;
+	        }
+	  }
+
+}
