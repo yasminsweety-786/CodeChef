39b57d12e6779c7edf194e7f6ea66e47b8e55b17
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-19 09:05:37 +0530

    CodeChef: MOVIE2X - Accepted Solution

---
+++ CodeChef/MOVIE2X.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int x,y;
+ cin>>x>>y;
+ int twice = y/2;
+ int rem = x-y;
+  int sum = rem +twice;
+   cout << sum<<endl;
+ 
+}
