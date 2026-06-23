b86dc2cba9906df99e89b79f3b7e1f6cd89b404c
yasmin_shaik <yasminsweety410@gmail.com>
2026-06-23 08:08:17 +0530

    CodeChef: FINDSHOES - Accepted Solution

---
+++ CodeChef/FINDSHOES.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+int t;
+ cin>>t;
+  while(t--)
+ {
+      int x,y;
+      cin>>x>>y;
+      int xtra = x+ max (0,x-y);
+      cout<<xtra<<endl;
+      
+      
+ }
+}
