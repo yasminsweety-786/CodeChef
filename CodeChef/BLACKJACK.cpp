0a1145ed6c7076d25af09774c0b68be59e7da923
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-06 04:06:24 +0530

    CodeChef: BLACKJACK - Accepted Solution

---
+++ CodeChef/BLACKJACK.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; 
+	 cin>> t;
+	  while (t--){
+	       int a ,b;
+	        cin>>a>>b;
+	        int c = 21-(a+b);
+	        if ( c>=1 && c<=10){ 
+	            cout<<c<<endl;
+	            
+	  }
+	  else {
+	       cout<<-1<<endl;
+	  }
+
+}
+}
