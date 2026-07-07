8bc9d337364353e14b682b57c62f0f5964be45fc
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-07 07:51:26 +0530

    CodeChef: FILLCANDIES - Accepted Solution

---
+++ CodeChef/FILLCANDIES.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	 while (t--){
+	     int n,k,m;
+	      cin>>n>>k>>m;
+	      int c = k*m ;
+	      int b = (n+c-1)/c;
+	      cout<<b<<endl;
+	 }
+	 
+
+}
