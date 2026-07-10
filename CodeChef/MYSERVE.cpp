756b82730039392b397c581cc81aad1d2b71edf1
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-10 14:42:26 +0530

    CodeChef: MYSERVE - Accepted Solution

---
+++ CodeChef/MYSERVE.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; 
+	cin>>t;
+while (t--)
+{
+     int x,y;
+      cin>>x>>y;
+  if (((x+y)/2 )%2 ==0){
+      cout<<"Alice"<<endl;
+  }
+  else {
+       cout<<"Bob"<<endl;
+  }
+
+}
+
+}
