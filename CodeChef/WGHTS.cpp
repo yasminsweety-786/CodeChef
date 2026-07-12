3b7ea4777f8774a630ff0417045891f42cd9a7b4
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-12 09:27:55 +0530

    CodeChef: WGHTS - Accepted Solution

--- CodeChef/WGHTS.cpp
+++ CodeChef/WGHTS.cpp
@@ -8,7 +8,7 @@ int t;
   while( t--){
        int w,x,y,z;
        cin>>w>>x>>y>>z;
-       if ( w== x||w== y|| w==z|| w == x+y ||w== x+z|| w== z+x){
+       if ( w== x||w== y|| w==z|| w == x+y ||w== y+z|| w== z+x|| w==x+y+z){
            cout<<"Yes"<<endl;
        }
       
