4bde29de37843165aadff8f86676b1883f0f04b2
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-14 07:19:02 +0530

    CodeChef: CHEFEREN - Accepted Solution

---
+++ CodeChef/CHEFEREN.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	 cin>>t;
+	 while(t--){
+	      int n,a,b;
+	       cin>>n>>a>>b;
+	       int e = n/2;
+	       int o = (n+1)/2;
+	       cout<< e*a + o*b<<endl;
+	        
+	       
+	 }
+
+}
