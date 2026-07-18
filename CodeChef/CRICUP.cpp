6223bb4446fd20e558078a71bdd27493d29bfeff
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-18 13:34:19 +0530

    CodeChef: CRICUP - Accepted Solution

---
+++ CodeChef/CRICUP.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int t; 
+	 cin>>t;
+	  while ( t--){
+	       int x,y,d;
+	        cin>>x>>y>>d;
+	        if ( x-y <= d){
+	             cout<<"YES"<<endl;
+	        }
+	        else {
+	             cout<<"NO"<<endl;
+	        }
+	  }
+
+}
