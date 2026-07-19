64b501940e08f9a0301b3c5c91efe215fc9bbbdd
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-19 08:40:57 +0530

    CodeChef: FLOORS - Accepted Solution

---
+++ CodeChef/FLOORS.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int t;
+	 cin>>t;
+	  while (t--){
+	       int x,y;
+	        cin>>x>>y;
+	       int   flrX = (x-1)/10+1;
+	        int   flrY = ( y-1)/10+1;
+	         cout<<abs( flrX -  flrY)<<endl;
+	         
+	        
+	  }
+
+}
