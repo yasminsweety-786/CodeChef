5c4cc993ea6a792559a261d2de094efb25b3326a
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-19 09:53:59 +0530

    CodeChef: GDTURN - Accepted Solution

---
+++ CodeChef/GDTURN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        // your code goes here
+        if (x+y>6){
+           cout<<"Yes"<<endl;
+        }
+         else {
+              cout<<"No"<<endl;
+         }
+    }
+	return 0;
+}
