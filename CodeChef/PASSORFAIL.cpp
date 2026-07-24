ceb41b62f9b5599a39cd1f8bfc5d81e1e48ea2fe
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-24 08:19:21 +0530

    CodeChef: PASSORFAIL - Accepted Solution

---
+++ CodeChef/PASSORFAIL.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int t;
+	  cin>>t;
+	   while ( t--){
+	        int n,x,p;
+	         cin>>n>>x>>p ;
+	       //  int incorrect =n-x;
+	          if ( p<= (3*x)-(n-x)){
+	               cout<<"Pass"<<endl;
+	          }
+	          else 
+	          
+	          {
+	               cout<<"Fail"<<endl;
+	          }
+	   }
+
+}
