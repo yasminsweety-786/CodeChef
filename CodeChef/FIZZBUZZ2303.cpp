b947dec5aa7b2a6e30bdfc086bcd267380441185
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-27 08:42:31 +0530

    CodeChef: FIZZBUZZ2303 - Accepted Solution

---
+++ CodeChef/FIZZBUZZ2303.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int t; 
+	 cin>>t;
+	  while(t--){
+	      int n;
+	       cin>>n;
+	       if (n> 2){
+	            cout<<n*2<<endl;
+	       }
+	       else{
+	            cout<<2<<endl;
+	       }
+	       
+	  }
+
+}
