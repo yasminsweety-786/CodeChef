b75427e29786692d0334764e778bd03ae8299f02
yasmin_shaik <yasminsweety410@gmail.com>
2026-07-30 11:07:01 +0530

    CodeChef: PRESENTS - Accepted Solution

---
+++ CodeChef/PRESENTS.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int t;
+	  cin>>t;
+	   while(t--){
+	        int n;
+	        cin>>n;
+	         if ( n>4){
+	              cout<<n-1<<endl;
+	         }
+	         else {
+	              cout<<n<<endl;
+	         }
+	   }
+
+}
