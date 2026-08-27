3d6c4e92934e14f56d888427fb0acbcb14f56d31
yasmin_shaik <yasminsweety410@gmail.com>
2026-08-27 09:02:53 +0530

    CodeChef: PYPRACREAR1A - Accepted Solution

---
+++ CodeChef/PYPRACREAR1A.cpp
@@ -0,0 +1,4 @@
+Since the code is accepting input as input() - the default value is assumed to be a string.
+Hence '1' and '2' will be treated as strings.
+'1' + '2' will be treated as string concatenation.
+Our output will be 12.
