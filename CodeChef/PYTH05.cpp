2082df93ed2f1177707149f223700743bcca6aab
yasmin_shaik <yasminsweety410@gmail.com>
2026-09-09 07:25:29 +0530

    CodeChef: PYTH05 - Accepted Solution

---
+++ CodeChef/PYTH05.cpp
@@ -0,0 +1,4 @@
+1. The first option is missing the parantheses (brackets) and the quotes to enclose the text "Hi"
+2. The second option doesn't enclose the text within quotes
+3. The third option is missing parantheses (brackets)
+4. The fourth option follows all the rules to write a print statement to print a text "Hi", this makes it the correct option
