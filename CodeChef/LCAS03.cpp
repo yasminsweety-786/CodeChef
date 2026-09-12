500133f06f6af1755f8272ab840752c0f99862f4
yasmin_shaik <yasminsweety410@gmail.com>
2026-09-12 08:51:39 +0530

    CodeChef: LCAS03 - Accepted Solution

---
+++ CodeChef/LCAS03.cpp
@@ -0,0 +1,6 @@
+The two printf statements will print consecutively.
+
+In the code snippet, printf("Hello, "); prints "Hello, " to the console.
+Then, printf("world!"); prints "world!" to the console.
+
+Since there is no newline character ('\n') at the end of either printf() statement, the output will appear on the same line.
