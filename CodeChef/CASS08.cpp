697decefee999a4ef1df52982565ae3643b4cf9b
yasmin_shaik <yasminsweety410@gmail.com>
2026-09-16 12:23:22 +0530

    CodeChef: CASS08 - Accepted Solution

---
+++ CodeChef/CASS08.cpp
@@ -0,0 +1,2 @@
+The printf function is used to print the string "Hello\nAnd Welcome John".
+\n is the escape sequence for a newline character, so "Hello" and "And Welcome John" will be printed on separate lines.
